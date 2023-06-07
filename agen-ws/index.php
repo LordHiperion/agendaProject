2eb39b4cfb1fc1964ab895945e1a90162ead5a82
Weslley Mendes <weslley.mendes@extremedigital.com.br>
2023-06-07 12:54:39 -0300

    adicionando o codigo base

--- agen-ws/index.php
+++ agen-ws/index.php
@@ -1,58 +1,46 @@
 <?php
-require_once 'repository/usuario-repository.php';
-require_once 'repository/agenda-repository.php';
-require_once 'model/Usuario.php';
-require_once 'model/Agenda.php';
-header('Content-Type: application/json; charset=utf-8');
-/* Connect to a MySQL database using driver invocation */
-
-// $dbh = new DatabaseConnection();
-$usuario_repository = new UsuarioRepository();
-$agenda_repository = new AgendaRepository();
-// $sql = 'show tables';
-// $sql = 'select * from Usuario';
-// foreach ($usuario_repository->getAll() as $row) {
-//     // var_dump($row);
-//     // var_dump($row['id']);
-//     // var_dump($row['nome']);
-//     // echo('<br>');
-//     // echo('<br>');
-//     $usuario = new Usuario($row['id'], $row['nome']);
-//     $usuarios = [
-//         $usuario,
-//         $usuario,
-//     ];
-//     // var_dump($usuarios);
-//     $json_obj = json_encode($usuarios);
-//     // $json_obj = get_object_vars($usuario);
-//     echo($json_obj);
-//     // echo('<br>');
-//     // echo('<br>');
-//     // var_dump(new Usuario($row['id'], $row['nome']));
+// require_once 'google-api-php-client--PHP5.6/vendor/autoload.php';
+// require_once 'repository/usuario-repository.php';
+// require_once 'repository/agenda-repository.php';
+// require_once 'repository/telefone-repository.php';
+// require_once 'model/Usuario.php';
+// require_once 'model/Agenda.php';
+// include_once 'model/Telefone.php';
+// require_once 'auth/auth.php';
+// header('Content-Type: application/json; charset=utf-8');
+// header("Access-Control-Allow-Origin: *");
+// // TODO: deixar esse método num outro arquivo que compartilha para todos os outros
+// function json_encode_unicode($data) {
+//     return json_encode($data, JSON_UNESCAPED_UNICODE);
 // }
-// $agendas = [];
-// foreach ($agenda_repository->getAll() as $row) {
-    //     $agenda = new Agenda();
-    
-    //     $agenda->id = $row['id'];
-    //     $agenda->sobre_nome = $row['sobre_nome'];
-    //     $agenda->email = $row['email'];
-    //     $agenda->observacao = $row['observacao'];
-    //     $agenda->usuario_id = $row['usuario_id'];
-    
-    //     $agendas[] = $agenda;
-    //     // $usuario = new Usuario($row['id'], $row['nome']);
-    //     // $usuarios = [
-        //     //     $usuario,
-        //     //     $usuario,
-//     // ];
-//     // $json_obj = json_encode($usuarios);
-//     // $json_obj = get_object_vars($usuario);
-//     // echo($json_obj);
-//     // echo('<br>');
-//     // echo('<br>');
-//     // var_dump(new Usuario($row['id'], $row['nome']));
-// }
+// // $usuario_repository = new UsuarioRepository();
+// $agenda_repository = new AgendaRepository();
+// $telefone_repository = new TelefoneRepository();
 
-// echo(json_encode($agendas));
-echo(json_encode($agenda_repository->getAll()));
+// $CLIENT_ID = "611415641517-7eke6sip127lv6obdiv1mnc17uuaupe0.apps.googleusercontent.com";
+// $CLIENT_ID = "CLIENT_ID_GOOGLE";
+// $headers = apache_request_headers();
+// if (isset($headers['Authorization'])) {
+//   $auth = $headers['Authorization'];
+
+//   preg_match('/Bearer\s+(.*)/', $auth, $matches);
+//   $token = $matches[1];
+//   $client = new Google_Client(['client_id' => $CLIENT_ID]);  // Specify the CLIENT_ID of the app that accesses the backend
+//   $payload = $client->verifyIdToken($token);
+
+// }
+// if (isset($payload)) {
+//   $userid = $payload['sub'];
+//   // If request specified a G Suite domain:
+//   //$domain = $payload['hd'];
+// //    echo('válido');
+// //    var_dump($payload);
+// // USE O sub COMO IDENTIFICADOR DO USUÁRIO
+// //    echo($payload['sub']);
+// } else {
+//   echo("Invalid ID token");
+// }
+// $payload['sub']
+// echo(json_encode_unicode($payload));
+
+// echo(json_encode_unicode($agenda_repository->getAll()));
--- agen-ws/model/Usuario.php
+++ agen-ws/model/Usuario.php
@@ -3,8 +3,16 @@
         public $id;
         public $nome;
 
-        public function __construct($id, $nome) {
-            $this->id = intval($id);
-            $this->nome = $nome;
+        // public function __construct($id, $nome) {
+        //     $this->id = intval($id);
+        //     $this->nome = $nome;
+        // }
+
+        public static function fromJson($json) {
+            $obj = json_decode($json, true);
+            $usuario = new Usuario();
+            $usuario->id = isset($obj['id']) ? $obj['id'] : null;;
+            $usuario->nome = isset($obj['nome']) ? $obj['nome'] : null;;
+            return $usuario;
         }
     }
