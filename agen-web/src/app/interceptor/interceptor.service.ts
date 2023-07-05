b201f953068530788c605205a58c5e78f789ef64
Fernando <jk157_372@hotmail.com>
2023-07-05 19:43:30 -0300

    terminado o interceptor e alterações no agenda service no qual não é necessario fazer a requisição de token.

--- agen-web/src/app/interceptor/interceptor.service.ts
+++ agen-web/src/app/interceptor/interceptor.service.ts
@@ -11,12 +11,22 @@ from'@angular/common/http';
 
 import { Observable } from 'rxjs/internal/Observable';
 
-@Injectable()
+@Injectable({
+  providedIn: 'root'
+})
 
 export class Interceptor implements HttpInterceptor {
   intercept( request: HttpRequest<any>, next: HttpHandler ):
   Observable<HttpEvent<any>> {
-    console.log(request);
+    // a session storage token nao estava definida na hora que era chamado o login.php ele só seta a session storage se o login.php der sucesso.
+    if (!request.url.includes('/login.php')) {
+      const token = sessionStorage.getItem('token');
+        request = request.clone({
+          setHeaders: {
+            Authorization:`Bearer ${token}`
+          }
+        });
+    }
     return next.handle(request);
   }
 }
--- agen-web/src/core/service/agenda.service.ts
+++ agen-web/src/core/service/agenda.service.ts
@@ -11,15 +11,12 @@ export class AgendaService {
 constructor(private http: HttpClient) { }
 
 getAll(): Observable<any>{
-  const token = sessionStorage.getItem('token');
-  const headers= new HttpHeaders({"Authorization": `Bearer ${token}`});
-  return this.http.get ('https://whertz.com.br/agenda_projeto/agenda_controller.php', {headers : headers})
+  return this.http.get ('https://whertz.com.br/agenda_projeto/agenda_controller.php' )
 }
 
 cadastrar(agenda: any) {
-  const token = sessionStorage.getItem('token');
-  const headers = new HttpHeaders({"Authorization": `Bearer ${token}`});
-  return this.http.post('https://whertz.com.br/agenda_projeto/agenda_controller.php', agenda, {headers : headers});
+
+  return this.http.post('https://whertz.com.br/agenda_projeto/agenda_controller.php', agenda);
 
 }
 
