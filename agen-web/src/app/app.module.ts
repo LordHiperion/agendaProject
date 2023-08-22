02af33afeb76d7ab50c9ab905d1f41a9209a1a2d
Fernando <jk157_372@hotmail.com>
2023-08-22 18:53:06 -0300

    iniciado a adição do input dinamico no form group com mascara para o telefone do contato da agenda

--- agen-web/src/app/app.module.ts
+++ agen-web/src/app/app.module.ts
@@ -11,6 +11,7 @@ import { LoginComponent } from './login/login.component';
 import { LogoutComponent } from './logout/logout.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { InterceptorModule } from './interceptor/interceptor.module';
+import { NgxMaskModule } from 'ngx-mask';
 
 @NgModule({
   declarations: [
@@ -26,6 +27,7 @@ import { InterceptorModule } from './interceptor/interceptor.module';
     HttpClientModule,
     ReactiveFormsModule,
     InterceptorModule,
+    NgxMaskModule.forRoot()
 
 
   ],
